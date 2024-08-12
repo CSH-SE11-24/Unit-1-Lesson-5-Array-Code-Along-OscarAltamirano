8ad9e4f297c52c16f1fb54db44da003ecb6392f0
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 12:00:49 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Declare a variable that stores an array of any 3 fictional people
+
+// Console log the first element of the array (using array name and index)
+
+// Console log the last element of the array (using array name and index)
+
+// Console log the last element of the array WITHOUT using the index -- use the array length instead
+
+// Change the second element of the array to be a different fictional character
+
+// Stretch: try ADDING a new fictional character to the array
+
